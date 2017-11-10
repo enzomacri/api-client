ca0cfa12676bc969d77f6769286bbd002ebe94dc
Enzo Macri <enzo.macri@netatmo.com>
2017-11-10 15:20:15 +0100

    Client.js: fixing Oauth part: sending url encoded post request

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
+'use strict';
 const Utils = require('./helpers/utils');
-const request = require('request');
+const request = require('./request');
 
 var Client = function(apiUrl, config) {
 
@@ -7,24 +8,24 @@ var Client = function(apiUrl, config) {
     this.client_id  = config.client_id;
     this.client_secret = config.client_secret;
 
-    if(config.access_token) this.access_token = config.access_token;
-    if(config.refresh_token) this.refresh_token = config.refresh_token;
+    if (config.access_token) this.access_token = config.access_token;
+    if (config.refresh_token) this.refresh_token = config.refresh_token;
 
-    if(config.authorization_code) this.authorization_code = config.authorization_code;
+    if (config.authorization_code) this.authorization_code = config.authorization_code;
 
-    if(config.username && config.password) {
+    if (config.username && config.password) {
         this.username = config.username;
         this.password = config.password;
     }
 
-    if(config.scopes) {
-        if(!Array.isArray(config.scopes)) {
+    if (config.scopes) {
+        if (!Array.isArray(config.scopes)) {
             config.scopes = [config.scopes];
         }
         this.scopes = config.scopes;
     }
 
-    if(config.urls) {
+    if (config.urls) {
         this.urls = config.urls;
     }
     else {
@@ -34,7 +35,7 @@ var Client = function(apiUrl, config) {
             authorize: apiUrl + '/authorize'
         };
 
-        if(config.apiSuffix) {
+        if (config.apiSuffix) {
             this.urls.api += config.apiSuffix;
         }
     }
@@ -42,14 +43,14 @@ var Client = function(apiUrl, config) {
 };
 
 Client.prototype.setTokens = function(tokens) {
-    if(tokens.access_token) {
+    if (tokens.access_token) {
         this.access_token = tokens.access_token;
     }
-    if(tokens.refresh_token) {
+    if (tokens.refresh_token) {
         this.refresh_token = tokens.refresh_token;
     }
-    if(tokens.expiration_token) {
-        this.expiration_token = tokens.expires_in;
+    if (tokens.expires_in) {
+        this.token_expiration = Utils.getTime() + tokens.expires_in;
     }
 };
 
@@ -60,98 +61,103 @@ Client.prototype.setAccessToken = function(accessToken) {
 Client.prototype.getTokens = function() {
     return {
         access_token: this.access_token,
-        expires_in: this.expiration_token,
+        expires_in: this.token_expiration - Utils.getTime(),
         refresh_token: this.refresh_token
     };
 };
 
-Client.prototype.makeRequest = function(url, params, method, callback) {
-    let headers = {};
+Client.prototype.makeRequest = function(url, params, options, callback) {
+    var headers = {};
+    var method = 'get';
 
-    if(!Utils.isObject(params)) {
-        if(typeof params === 'function') {
+    if (!Utils.isObject(params)) {
+        if (typeof params === 'function') {
             callback = params;
             params = {};
-            method = 'get';
-        }
-        else if (typeof params === 'string') {
-            callback = method;
-            method = params;
         }
     }
-    if(typeof method === 'function') {
+    if (typeof method === 'function') {
         callback = method;
-        method = 'get';
     }
+    if (Utils.isObject(options)) {
+        if (options.method) {
+            method = options.method;
+        }
 
-    if(params.access_token) {
+        if (options.content_type) {
+            headers['Content-Type'] = options.content_type;
+        }
+    }
+    if (params.access_token) {
         headers.authorization = 'Bearer ' + params.access_token;
     }
-    var options = {
-        uri: url
-    };
 
-    if(method === 'get') {
-        options.qs = params;
-    }
-    else {
-        options = Object.assign(options, {
-            json: true,
-            body: params
-        });
-    }
-    request[method](options, (err, response) => {
-        return Utils.handleCallbackOrPromise(callback, err, response)
+    request[method](url, params, headers, (err, response) => {
+        if (err) {
+            return callback(err);
+        }
+        if (response.body && !Utils.isObject(response.body)) {
+            try {
+                response.body = JSON.parse(response.body);
+            } catch(err) {
+               return callback(new Error('Unable to parse response'));
+            }
+        }
+
+        if (response.body.error && response.body.error.message) {
+            return callback(new Error(response.body.error.message));
+        }
+
+        return callback(null, response);
     });
 };
 
-Client.prototype.makeOAuth2Request = function(endpoint, params, method, callback) {
+Client.prototype.makeOAuth2Request = function(endpoint, params, options, callback) {
 
-    if(!Utils.isObject(params)) {
-        if(typeof params === 'function') {
+    if (!Utils.isObject(params)) {
+        if (typeof params === 'function') {
             callback = params;
-            method = 'get';
-        }
-        else if (typeof params === 'string') {
-            callback = method;
-            method = params;
+            options= {};
         }
         params = {};
     }
-    if(typeof method === 'function') {
-        callback = method;
-        method = 'get';
+    if (typeof options === 'function') {
+        callback = options;
+        options= {};
     }
 
-    params.access_token = this.access_token;
     this.getAccessToken((err) => {
-        if(err) {
+        if (err) {
             // TODO if token is invalid try to refresh it
-            if(err.status === 401) {
+            if (err.status === 401) {
                 this.refreshAccessToken((err) => {
-                    if(err) return Utils.handleCallbackOrPromise(callback, err);
-                    else return this.makeRequest(this.urls.api + endpoint, params, method, callback);
+                    if (err) {
+                        return callback(err);
+                    } else  {
+                        params.access_token = this.access_token;
+                        return this.makeRequest(this.urls.api + endpoint, params, options, callback);
+                    }
                 });
             }
-            else return Utils.handleCallbackOrPromise(callback, err);
+            else return callback(err);
         }
-        return this.makeRequest(this.urls.api + endpoint, params, method, callback);
+        params.access_token = this.access_token;
+        return this.makeRequest(this.urls.api + endpoint, params, options, callback);
     });
 };
 
 Client.prototype.get = function(endpoint, params, callback) {
-    return this.makeOAuth2Request(endpoint, params, 'get', callback);
+    return this.makeOAuth2Request(endpoint, params, {method: 'get'}, callback);
 };
 
 Client.prototype.post = function(endpoint, params, callback) {
-    return this.makeOAuth2Request(endpoint, params, 'post', callback);
+    return this.makeOAuth2Request(endpoint, params, {method:'post'}, callback);
 }
 
 function getTokensFromUserCredentials(cb) {
-    if(!this.username || !this.password || !this.urls.token) {
+    if (!this.username || !this.password || !this.urls.token) {
         var err = new Error('Missing args to perform user credentials authentication');
-        if(typeof cb === 'function') return cb(err);
-        else return Promise.reject(err);
+        return cb(err);
     }
     var params = {
         grant_type: 'password',
@@ -159,21 +165,23 @@ function getTokensFromUserCredentials(cb) {
         client_secret: this.client_secret,
         username : this.username,
         password: this.password,
-        scope: this.scopes
+        scope: this.scopes.join()
     };
-    console.log(params)
-    return this.makeRequest(this.urls.token, params,'post', (err, res) => {
-        if(err) return Utils.handleCallbackOrPromise(cb, err);
-        this.setTokens(res);
-        return Utils.handleCallbackOrPromise(cb, null, res);
+    var config = {
+        method : 'post',
+        content_type :"application/x-www-form-urlencoded"
+    };
+    return this.makeRequest(this.urls.token, params, config, (err, res) => {
+        if (err) return cb(err);
+        this.setTokens(res.body);
+        return cb(null, res);
     });
 };
 
 function refreshAccessToken(cb) {
-    if(!this.refresh_token || !this.urls.token) {
+    if (!this.refresh_token || !this.urls.token) {
         var err = new Error('Missing args for refreshing tokens');
-        if(typeof cb === 'function') return cb(err);
-        else return Promise.reject(err);
+        return cb(err);
     };
 
     var params = {
@@ -182,38 +190,41 @@ function refreshAccessToken(cb) {
         client_secret: this.client_secret,
         refresh_token : this.refresh_token
     };
+
+    var config = {
+        'content_type': "application/x-www-form-urlencoded",
+        'method' : 'post'
+    };
     return this.makeRequest(this.urls.token, params, (err, res) => {
-        if(err) return Utils.handleCallbackOrPromise(cb, err);
+        if (err) return cb(err);
         this.setTokens(res);
-        return Utils.handleCallbackOrPromise(cb, null, res);
+        return cb(null, res);
     });
 };
 
 // TODO
 function getTokensFromAuthorizationCode(cb) {
-    if(!this.authorization_code) {
+    if (!this.authorization_code) {
         var err = new Error("Missing args");
-        return Utils.handleCallbackOrPromise(cb, err);
+        return cb(err);
     }
 };
 
 Client.prototype.getAccessToken = function(callback) {
-
-    if(this.access_token && this.expiration_token && this.expiration_token < Utils.getTime()) {
+    if (this.access_token && this.token_expiration && this.token_expiration > Utils.getTime()) {
         return callback(null, this.access_token);
     }
-    if(this.authorization_code) {
+    if (this.authorization_code) {
         return getTokensFromAuthorizationCode.call(this, callback);
     }
-    if(this.refresh_token) {
+    if (this.refresh_token) {
         return refreshAccessToken.call(this, callback);
     }
-    if(this.username && this.password) {
+    if (this.username && this.password) {
         return getTokensFromUserCredentials.call(this, callback);
     }
     var err = new Error('Unable to retrieve access token');
-    if(typeof callback === 'function') return callback(err);
-    else return Promise.reject(err);
+    if (typeof callback === 'function') return callback(err);
 }
 
 module.exports = Client;
